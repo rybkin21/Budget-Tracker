f9c4603d6e64829ae93fa67b474a16ce523321eb
Ivan Rybkin <i.rybkin@icloud.com>
2024-11-03 18:30:11 +0300

    Initial Commit

---
+++ Budget Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budget Tracker
+//
+//  Created by Ivan Rybkin on 03.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
